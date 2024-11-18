dfebc5d0065046bb4ec2b00b792d5711676b389c
Online <mailonline95@gmail.com>
2024-11-18 19:20:22 +0500

    commit-7

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,11 +1,14 @@
-import Image from "next/image";
+import './../styles.scss';
 import './../styles/components/_header.scss';
+import Image from "next/image";
 import Button from "./Button";
+import Link from "next/link";
 
 export default function Header() {
   return (
     <>
         <section className="section">
+
             <div className="header">
                 <div className="header__container">
                     <div className="header__logo">
@@ -22,13 +25,47 @@ export default function Header() {
                     </div>
                     <div className="header__find">
                         <Button style={"button button__location header__button-location"}>
-                            <img src="./placeholder.png" alt="" />
+                            <img src="./placeholder.png"/>
                             Выберите адрес
                         </Button>
                     </div>
+                    <Button style={'button button__login'}>Войти</Button>
                 </div>
-                <Button style={'button button__login'}>Войти
-                </Button>
+            </div>
+
+            <div className="header-mobile">
+                <div className="header-mobile__container">
+                    <div className="header-mobile__burger-menu">
+                        <input id="menu__toggle" type="checkbox" />
+                        <label className="menu__btn" htmlFor="menu__toggle"></label>
+                    </div>
+                    <Button style={'button'}>Позвонить</Button>
+                    <Image
+                        src="./mobileLogo.svg"
+                        width={30}
+                        height={21}
+                        alt="logo"
+                    />
+                    <Image
+                        src="/logout.png"
+                        width={24}
+                        height={24}
+                        alt="logo"
+                    />
+                </div>
+                <ul className="menu__box">
+                    <li><Link href={"#"}>Войти</Link></li>
+                    <li><Link href={"#"}>Павлодар</Link></li>
+                    <li><Link href={"#"}>Русский</Link></li>
+                    <li><Link href={"#"}>Политика конфиденциальности</Link></li>
+                    <li><Link href={"#"}>Согласие на обработку ПД</Link></li>
+                    <li><Link href={"#"}>Контактная и правовая информация</Link></li>
+                    <li><Link href={"#"}>Акции</Link></li>
+                    <li><Link href={"#"}>Программа лояльности “Ёби-клуб”</Link></li>
+                    <li><Link href={"#"}>Доставка и оплата</Link></li>
+                    <li><Link href={"#"}>О компании</Link></li>
+                    <li><Link href={"#"}>+7 747 095 83 04</Link></li>
+                </ul>
             </div>
         </section>
     </>
