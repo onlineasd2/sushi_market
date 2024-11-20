3ab7a25c69d7db3421e8bcdb339fda9f6e963855
onlineasd2 <mailonline95@gmail.com>
2024-11-20 12:13:25 +0500

    card-1

---
+++ src/app/components/Card.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Card() {
+  return (
+    <>
+        <div className="card">
+            <h4>Пицца</h4>
+            <p>asdasd</p>
+        </div>
+    </>
+  )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Category from "./components/Category";
 import Header from "./components/Header";
+import Card from "./components/Card";
 
 export default function Home() {
   return (
     <>
       <Header />
       <Category/>
+      <Card />
     </>
   );
 }
