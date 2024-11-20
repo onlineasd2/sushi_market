2698f84c6631367976e731e1e4f09df9511e3176
onlineasd2 <mailonline95@gmail.com>
2024-11-20 17:34:05 +0500

    card-3

--- src/app/components/Card.tsx
+++ src/app/components/Card.tsx
@@ -1,20 +1,24 @@
 import React from 'react'
 import './../styles/components/_card.scss';
 import Button from './Button'
+import Image from 'next/image';
 
 export default function Card() {
   return (
     <>
         <div className="card">
-            <img 
-                src="./sushi-card1.webp"
-                width="262px"
-                height="262px"
+            <Image
+                src="/sushi-card1.png"
+                width={262}
+                height={262}
                 className='card__image'
-                alt="" />
+                alt="">
+            </Image>
             <div className="card__content">
-                <h4 className='card__title'>Куни Ли Лососёвый</h4>
-                <p className='card__year'>2010 г</p>
+                <div className="card__option">
+                    <h4 className='card__title'>Куни Ли Лососёвый</h4>
+                    <p className='card__year'>2010 г</p>
+                </div>
                 <p className='card__description'>Вот это улов! Добавили в «Куни Ли Легендарный» ещё два ролла с рыбой и получили идеальный морской сет для твоей встречи. Состав: Цезарь темпура, Лава темпура, Цезарь BIG, Филадельфия лайт, Краб терияки, Киото, Яркий лосось, Лосось с тобико</p>
                 <div className="card__option">
                     <h3 className='card__price'><b>12 990 ₸</b></h3>
