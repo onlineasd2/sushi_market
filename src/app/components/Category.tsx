2f6d0fa3a8826ad965893d7fba5a168904370550
onlineasd2 <mailonline95@gmail.com>
2024-11-22 15:06:34 +0500

    fix-layout-3

--- src/app/components/Category.tsx
+++ src/app/components/Category.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import './../styles/components/_category.scss';
 import Link from 'next/link';
-import Image from 'next/image';
 
 export default function Category() {
   return (
