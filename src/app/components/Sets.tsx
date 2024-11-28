3da236ed88795a84f034564866c7c3cdffd3fea1
onlineasd2 <mailonline95@gmail.com>
2024-11-28 12:27:21 +0500

    pagination 1

--- src/app/components/Sets.tsx
+++ src/app/components/Sets.tsx
@@ -8,53 +8,64 @@ import sushiApi from './../services/sushiApi';
 import Button from './Button';
 
 interface SetsProps {
-  titleMain: string;
-  cards: ICard[];
+    titleMain: string;
+    cards: ICard[];
 }
 
 const Sets: React.FC<SetsProps> = ({ titleMain }) => {
 
-  const [sets, setSets] = useState<ICard[]>([]); // Карточки на одной странице
-  const [errorSets, setErrorSets] = useState('');
-  const [currentPage, setCurrentPage] = useState(1);
+    const [sets, setSets] = useState<ICard[]>([]); // Карточки на одной странице
+    const [errorSets, setErrorSets] = useState('');
+    const [currentPage, setCurrentPage] = useState(1);
+    const PAGE_LIMIT = 5;
 
-  const fetchData = async () => {
-    try {
-      const data = await sushiApi.getSushiSets(currentPage, 5);
-      setSets((prev) => [...prev, ...data]); 
-    } catch (error) {
-      setErrorSets(String(error));
+    const nextPage = () => {
+        setCurrentPage(currentPage + 1);
     }
-  };
 
-  const nextPage = () => {
-      setCurrentPage(currentPage + 1);
-  }
 
-  useEffect(() => {
-    fetchData()
-  }, [currentPage]);
-  
-  return (
-    <>
-        <div className="section">
-            <div className="wrapper">
-                <div className="sets">
-                  <h2 className='sets__title'><b>{titleMain}</b></h2>
-                  <div className="sets__container">
-                      {sets.map((set) => (
-                        <Card key={set.id} card={set} />
-                      ))}
-                  </div>
-                  <div className="sets__more">
-                        <Button onClick={nextPage} style="button button__login">Ещё</Button>
-                  </div>
-                  <h3>{errorSets}</h3>
+    const fetchSets = async () => {
+        try {
+            const data = await sushiApi.getSushiSets(currentPage, PAGE_LIMIT);
+            setSets(data);
+        } catch (error) {
+            setErrorSets(String(error));
+        }
+    }
+
+    useEffect(() => {
+        fetchSets()
+    }, [currentPage]);
+
+
+    useEffect(() => {
+        fetchSets();
+    }, []);
+
+    return (
+        <>
+            <div className="section">
+                <div className="wrapper">
+                    <div className="sets">
+                        <h2 className='sets__title'><b>{titleMain}</b></h2>
+                        {errorSets ? // Проверка на ошибки запроса fetchSets()
+                            <h3>{errorSets.toString()}</h3>
+                            :
+                            <div>
+                                <div className="sets__container">
+                                    {sets.map((set) => (
+                                        <Card key={set.id} card={set}/>
+                                    ))}
+                                </div>
+                                <div className="sets__more">
+                                    <Button onClick={nextPage} style="button button__login">Ещё</Button>
+                                </div>
+                            </div>
+                        }
+                    </div>
                 </div>
             </div>
-        </div>
-    </>
-  );
-}
+        </>
+    )};
 
 export default Sets;
