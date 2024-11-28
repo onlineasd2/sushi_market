edf977ff6ecb887987755c6a17baa4645a53c72e
onlineasd2 <mailonline95@gmail.com>
2024-11-28 12:27:21 +0500

    pagination 1

--- src/app/components/Sets.tsx
+++ src/app/components/Sets.tsx
@@ -5,45 +5,48 @@ import './../styles/components/_sets.scss';
 import Card from './Card';
 import ICard from '../interfaces/ICard';
 import sushiApi from './../services/sushiApi';
+import Button from './Button';
 
 interface SetsProps {
   titleMain: string;
+  cards: ICard[];
 }
 
 const Sets: React.FC<SetsProps> = ({ titleMain }) => {
 
-    const [sets, setSets] = useState<ICard[]>([]); // Карточки на одной странице
-    const [errorSets, setErrorSets] = useState('');
-    const limit = 5;
+  const [sets, setSets] = useState<ICard[]>([]); // Карточки на одной странице
+  const [errorSets, setErrorSets] = useState('');
+  const [currentPage, setCurrentPage] = useState(1);
 
-    const fetchSets = async () => {
-        try {
-            const data = await sushiApi.getSushiSets(1, limit);
-            setSets(data);
-        } catch (error) {
-            setErrorSets(String(error));
-        }
+  const fetchdata = async () => {
+    try {
+      const data = await sushiApi.getSushiSets(currentPage, 5);
+      setSets((prev) => [...prev, ...data]); 
+    } catch (error) {
+      setErrorSets(String(error));
+      throw error;
     }
+  };
 
-    useEffect(() => {
-        fetchSets();
-    }, []);
+  useEffect(() => {  
+    fetchdata();
+  }, [currentPage]);
   
   return (
     <>
         <div className="section">
             <div className="wrapper">
                 <div className="sets">
-                    <h2 className='sets__title'><b>{titleMain}</b></h2>
-                    {errorSets ? // Проверка на ошибки запроса fetchSets()
-                    <h3>{errorSets.toString()}</h3>
-                        :
-                    <div className="sets__container">
-                        {sets.map((set) => (
-                            <Card key={set.id} card={set}/>
-                        ))}
-                    </div>
-                    }
+                  <h2 className='sets__title'><b>{titleMain}</b></h2>
+                  <div className="sets__container">
+                      {sets.map((set) => (
+                        <Card key={set.id} card={set} />
+                      ))}
+                  </div>
+                  <div className="sets__more">
+                        <Button onClick={() => setCurrentPage(currentPage + 1)} style="button button__login">Ещё</Button>
+                  </div>
+                  <h3>{errorSets}</h3>
                 </div>
             </div>
         </div>
