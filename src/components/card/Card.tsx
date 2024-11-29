5392734c4ef76218f379c288921b754936a4ab25
onlineasd2 <mailonline95@gmail.com>
2024-11-29 14:16:58 +0500

    fsd-fix-2

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './styles.scss';
 import Image from 'next/image';
-import ICard from './../../interfaces/ICard';
+import ICard from '../sets/ICard';
 import Button from '../button/Button';
 
 
