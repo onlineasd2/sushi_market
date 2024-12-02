fb5a13284bcd10e88ab4f07fc282f430dc97c1a3
onlineasd2 <mailonline95@gmail.com>
2024-12-02 12:30:04 +0500

    pagination 1 fix 2

--- src/app/components/Sets.tsx
+++ src/app/components/Sets.tsx
@@ -5,6 +5,7 @@ import './../styles/components/_sets.scss';
 import Card from './Card';
 import ICard from '../interfaces/ICard';
 import sushiApi from './../services/sushiApi';
+import Button from './Button';
 
 interface SetsProps {
   titleMain: string;
@@ -17,18 +18,21 @@ const Sets: React.FC<SetsProps> = ({ titleMain }) => {
   const [errorSets, setErrorSets] = useState('');
   const [currentPage, setCurrentPage] = useState(1);
 
-  const fetchdata = async () => {
+  const fetchData = async () => {
     try {
       const data = await sushiApi.getSushiSets(currentPage, 5);
-      setSets((prev) => [...prev, ...data]);
+      setSets((prev) => [...prev, ...data]); 
     } catch (error) {
       setErrorSets(String(error));
-      throw error;
     }
   };
 
+  const nextPage = () => {
+      setCurrentPage(currentPage + 1);
+  }
+
   useEffect(() => {
-    fetchdata();
+    fetchData()
   }, [currentPage]);
   
   return (
@@ -36,16 +40,16 @@ const Sets: React.FC<SetsProps> = ({ titleMain }) => {
         <div className="section">
             <div className="wrapper">
                 <div className="sets">
-                    <h2 className='sets__title'><b>{titleMain}</b></h2>
-                    {errorSets ? // Проверка на ошибки запроса fetchSets()
-                    <h3>{errorSets.toString()}</h3>
-                        :
-                    <div className="sets__container">
-                        {sets.map((set) => (
-                            <Card key={set.id} card={set}/>
-                        ))}
-                    </div>
-                    }
+                  <h2 className='sets__title'><b>{titleMain}</b></h2>
+                  <div className="sets__container">
+                      {sets.map((set) => (
+                        <Card key={set.id} card={set} />
+                      ))}
+                  </div>
+                  <div className="sets__more">
+                        <Button onClick={nextPage} style="button button__login">Ещё</Button>
+                  </div>
+                  <h3>{errorSets}</h3>
                 </div>
             </div>
         </div>
