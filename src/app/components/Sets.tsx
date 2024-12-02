524df8e938f772324ce1e2f106518b9eb7ea6205
Online <mailonline95@gmail.com>
2024-12-02 20:35:50 +0500

    pagination 1 fix 4

--- src/app/components/Sets.tsx
+++ src/app/components/Sets.tsx
@@ -19,43 +19,53 @@ const Sets: React.FC<SetsProps> = ({ titleMain }) => {
   const [currentPage, setCurrentPage] = useState(1);
   const PAGE_LIMIT = 5;
 
-  const fetchData = async () => {
-    try {
-      const data = await sushiApi.getSushiSets(currentPage, PAGE_LIMIT);
-      setSets((prev) => [...prev, ...data]); 
-    } catch (error) {
-      setErrorSets(String(error));
-    }
-  };
-
   const nextPage = () => {
       setCurrentPage(currentPage + 1);
   }
 
-  useEffect(() => {
-    fetchData()
-  }, [currentPage]);
-  
+
+    const fetchSets = async () => {
+        try {
+            const data = await sushiApi.getSushiSets(currentPage, PAGE_LIMIT);
+            setSets(data);
+        } catch (error) {
+            setErrorSets(String(error));
+        }
+    }
+
+    useEffect(() => {
+        fetchSets()
+    }, [currentPage]);
+
+
+    useEffect(() => {
+        fetchSets();
+    }, []);
+
   return (
-    <>
+      <>
         <div className="section">
             <div className="wrapper">
                 <div className="sets">
-                  <h2 className='sets__title'><b>{titleMain}</b></h2>
-                  <div className="sets__container">
-                      {sets.map((set) => (
-                        <Card key={set.id} card={set} />
-                      ))}
-                  </div>
-                  <div className="sets__more">
-                        <Button onClick={nextPage} style="button button__login">Ещё</Button>
-                  </div>
-                  <h3>{errorSets}</h3>
+                    <h2 className='sets__title'><b>{titleMain}</b></h2>
+                    {errorSets ? // Проверка на ошибки запроса fetchSets()
+                        <h3>{errorSets.toString()}</h3>
+                        :
+                        <div>
+                            <div className="sets__container">
+                                {sets.map((set) => (
+                                    <Card key={set.id} card={set}/>
+                                ))}
+                            </div>
+                            <div className="sets__more">
+                                <Button onClick={nextPage} style="button button__login">Ещё</Button>
+                            </div>
+                        </div>
+                    }
                 </div>
             </div>
         </div>
     </>
-  );
-}
+)};
 
 export default Sets;
