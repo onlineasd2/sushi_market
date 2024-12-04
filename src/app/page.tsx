3d10762866b2cf68978a180ada862fc6ac2dd638
onlineasd2 <mailonline95@gmail.com>
2024-12-04 09:50:59 +0500

    fsd-fix-5

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import Category from "./../components/category/Category";
-import Header from "./../components/header/Header";
-import Sets from "./../components/sets/Sets";
-import Modal from "./../components/modal/Modal";
-import Slider from "./../components/slider/Slider";
-import Footer from "./../components/footer/Footer";
+import { Category } from "@/components/category/Category";
+import { Header } from "@/components/header/Header";
+import { Sets } from "@/components/sets/Sets";
+import { Modal } from "@/components/modal/Modal";
+import { Slider } from "@/components/slider/Slider";
+import { Footer } from "@/components/footer/Footer";
 
 export default function Home() {
   return (
