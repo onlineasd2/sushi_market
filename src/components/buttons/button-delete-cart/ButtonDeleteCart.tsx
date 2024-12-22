6310f2138f4e9f8fe44ae7fa1efc6340ba90e7d1
Online <mailonline95@gmail.com>
2024-12-22 23:34:13 +0500

    feat(cart): making-logic-cart-1

--- src/components/buttons/button-delete-cart/ButtonDeleteCart.tsx
+++ src/components/buttons/button-delete-cart/ButtonDeleteCart.tsx
@@ -1,18 +1,22 @@
 import React from "react";
 import "./styles.scss";
+import { db, Order } from "@/services/db";
 
 interface ButtonProps {
-    onClick?: () => void;
     children: React.ReactNode;
+    Order: Order | null;
 }
 
 export const ButtonDeleteCart = ({
-    onClick,
     children,
+    Order,
 }: ButtonProps): React.JSX.Element => {
+    const DeleteOrder = async (): Promise<void> => {
+        await db.orders.delete(Order?.id);
+    };
     return (
         <button
-            onClick={onClick}
+            onClick={DeleteOrder}
             className="button button__trash"
             tabIndex={0}
             aria-label="Кнопка"
--- src/services/db.ts
+++ src/services/db.ts
@@ -9,13 +9,22 @@ interface Order {
     price: number;
 }
 
+interface Sticks {
+    id?: number;
+    count: number;
+    price: number;
+}
+
 const db = new Dexie("OrdersDatabase") as Dexie & {
     orders: EntityTable<Order, "id">;
+    sticks: EntityTable<Sticks, "id">;
 };
 
 db.version(1).stores({
     orders: "++id, name, weight, key, count, price",
+    sticks: "++id, count, price",
 });
 
 export type { Order };
+export type { Sticks };
 export { db };
