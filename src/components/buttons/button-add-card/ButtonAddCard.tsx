8b8390ae428cf245b71949e566eb0d4156d99829
onlineasd2 <mailonline95@gmail.com>
2024-12-26 17:55:09 +0500

    refactor(buttons): making-props-drilling-4

--- src/components/buttons/button-add-card/ButtonAddCard.tsx
+++ src/components/buttons/button-add-card/ButtonAddCard.tsx
@@ -14,60 +14,10 @@ export const ButtonAddCard = ({
     // const DeleteOrder = async (): Promise<void> => {
     //     await db.orders.delete(id);
     // };
-    // const addDB = async () => {
-    //     try {
-    //         await db.orders.add({
-    //             name: order.title,
-    //             weight: order.weight,
-    //             key: order.id,
-    //             count: 1,
-    //             price: order.price,
-    //         });
-    //     } catch (error) {
-    //         console.error(error);
-    //     }
-    // };
-    //
+
     const onCart = () => {
         onChange?.(1);
     };
-
-    // const EditOrder = async (newOrderCount: number): Promise<void> => {
-    //     await db.orders.update(id, {
-    //         count: newOrderCount,
-    //     });
-    // };
-
-    //
-    // const handleAddCart = () => {
-    //     const newOrderCount = count + 1;
-    //     setCount(1);
-    //     EditOrder(newOrderCount);
-    //     AddOrder();
-    // };
-    //
-    // const GetStateButton = async () => {
-    //     try {
-    //         const res = await db.orders.where("key").equals(Order.key).first();
-    //         if (res?.key === Order.key) {
-    //             setCount(res.count);
-    //             if (res.id !== undefined) setId(res.id);
-    //             // else setStatus("Status res.id undefined");
-    //         } else setCount(0);
-    //     } catch (error) {
-    //         console.log(error);
-    //         // setStatus(`Error ${error}`);
-    //     }
-    // };
-    //
-    // useEffect(() => {
-    //     GetStateButton();
-    // }, []);
-    //
-    // useEffect(() => {
-    //     addDB();
-    //     console.log("count", count);
-    // }, [count);
 
     return value !== 0 ? (
         <ButtonCounter onChange={onChange} value={value} />
