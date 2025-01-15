ccd707f521c263c635c7f0f1a9d130fc0bfe18e1
Online <mailonline95@gmail.com>
2025-01-15 23:14:04 +0500

    fix(addressModal): added-global-order-var

--- src/components/sets/ICard.ts
+++ src/components/sets/ICard.ts
@@ -1,6 +1,8 @@
 export interface ICard {
     id: number;
     title: string;
+    count?: number;
+    key?: number;
     image: string;
     weight: number;
     description: string;
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from "./counterSlice";
+import ordersReducer from "./ordersSlice";
 
 export const store = configureStore({
     reducer: {
         cartCount: counterReducer,
+        cartOrders: ordersReducer,
     },
 });
 
